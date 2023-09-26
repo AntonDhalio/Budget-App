280cdd17a08dea25730e3d2fa1c2ada11f46da8c
AntonDhalio <83759822+AntonDhalio@users.noreply.github.com>
2023-09-26 09:04:35 +0200

    Add project files.

---
+++ Budget-App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Budget_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Budget-App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Budget_App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
